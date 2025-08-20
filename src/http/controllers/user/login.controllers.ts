eed56126c47215da37875774ef26f3037aea7a7c
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-08-19 22:37:23 -0300

    feat: adicionando middleware de autenticação para rotas

--- src/http/controllers/user/login.controllers.ts
+++ src/http/controllers/user/login.controllers.ts
@@ -1,5 +1,7 @@
 import { supabase } from "../../../config/supabase";
+import authMiddleware from "../../../middlewares/authentication/auth-middleware";
 import { userLoginSchema } from "../../../validators/user.validator";
+import jwt from "jsonwebtoken";
 
 export const userLogin: any = async (request: any, response: any) => {
   const body: any = request.body;
@@ -18,5 +20,17 @@ export const userLogin: any = async (request: any, response: any) => {
     return response.status(400).send({message: error.message});
   }
 
-  return response.status(200).send({message: "OK"});
+  const payload: any = {
+    id: data?.user?.id,
+    email: data?.user?.email,
+    name: data?.user?.user_metadata.name,
+  }
+
+  const newAccessToken: string = jwt.sign(payload, authMiddleware.MySecretWord, {expiresIn: "24h"});
+
+  return response.status(200).send({
+    message: "OK",
+    newAccessToken,
+    profile: payload,
+  });
 }
